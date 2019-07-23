1b11b5411d0193a182a09058ba14edcfc4a2443a
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-23 16:15:39 -0700

    Modifying timing of loading screen

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,13 +1,36 @@
 
-var count = 0;
+// console.log(document.cookie)
+
+// if(document.cookie.replace(/(?:(?:^|.*;\s*)visited\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true"){
+//     document.cookie = "randString";
+//     setTimeout(showStuff, 1900);
+// }
+// else{
+//     $('#mainContent').show();
+// }
+
+// function showStuff() {
+//     $('#loadContent').css("display", "none");
+//     $('#mainContent').fadeIn(2000);
+
+// }
+
+function doOnce() {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+        // setTimeout(showStuff, 1900);
+        document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+        setTimeout(showStuff, 1900);
+    }
+    else {
+        $('#loadContent').css("display", "none");
+        $('#mainContent').show();
+    }
+}
 
 function showStuff() {
     $('#loadContent').css("display", "none");
-    $('#mainContent').css("display", "unset");
-    count++;
-    console.log("count");
+    $('#mainContent').fadeIn(2000);
+
 }
 
-if (count === 0) {
-    setTimeout(showStuff, 3000);
-}
+doOnce();
