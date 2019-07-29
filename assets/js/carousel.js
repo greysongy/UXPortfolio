33b9bf90cd0671bb4b991c33b3b2da2bf6d1f4fb
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-28 17:15:49 -0700

    Added additional projects, plus iterative title

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -17,4 +17,89 @@ function rotate(e){
     "-o-transform": "rotateY("+currdeg+"deg)",
     "transform": "rotateY("+currdeg+"deg)"
   });
+  console.log("Current Degrees", currdeg);
+  if(currdeg >= 0) {
+    var remainder = currdeg%360;
+    console.log("remainder", remainder);
+    switch(remainder) {
+      case 0: 
+        console.log("Best Gig");
+        bestGig();
+        break;
+      case 60: 
+        console.log("Decision Tree");
+        decisionTree();
+        break;
+      case 120: 
+        console.log("Tetris");
+        tetris();
+        break;
+      case 180: 
+        console.log("Finfo");
+        finfo();
+        break;
+      case 240: 
+        console.log("PageRank");
+        pagerank();
+        break;
+      case 300: 
+        console.log("Othello");
+        othello();
+        break;
+    }
+  }
+  else if(currdeg < 0) {
+    var remainder = Math.abs(currdeg)%360;
+    console.log("neg remainder",remainder);
+    switch(remainder) {
+      case 0: 
+        console.log("Best Gig");
+        bestGig();
+        break;
+      case 60: 
+        console.log("Othello");
+        othello();
+        break;
+      case 120: 
+        console.log("PageRank");
+        pagerank();
+        break;
+      case 180: 
+        console.log("Finfo");
+        finfo();
+        break;
+      case 240: 
+        console.log("Tetris");
+        tetris();
+        break;
+      case 300: 
+        console.log("Decision Tree");
+        decisionTree();
+        break;
+    }
+  }
+
+  function bestGig() {
+    $("#projectText").text("Best Gig");
+  }
+
+  function decisionTree() {
+    $("#projectText").text("Decision Tree");
+  }
+
+  function tetris() {
+    $("#projectText").text("Tetris");
+  }
+
+  function finfo() {
+    $("#projectText").text("Finfo");
+  }
+
+  function pagerank() {
+    $("#projectText").text("PageRank");
+  }
+
+  function othello() {
+    $("#projectText").text("Othello");
+  }
 }
