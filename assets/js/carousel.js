f0e0833d581aeb6863e191e2eef3a02803ca840a
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-28 21:35:51 -0700

    Added shifting project descriptions

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -53,7 +53,7 @@ function rotate(e){
     console.log("neg remainder",remainder);
     switch(remainder) {
       case 0: 
-        console.log("Best Gig");
+        console.log("BestGig");
         bestGig();
         break;
       case 60: 
@@ -81,25 +81,45 @@ function rotate(e){
 
   function bestGig() {
     $("#projectText").text("Best Gig");
+    $("#codeShow").text("Git Repo: ");
+    $("#codeLink").attr("href", "https://github.com/greysongy/BestGig");
+    $("#codeLink").show();
+    $("#codeDescription").text("Application for gig workers to compare rates and get machine learning forecasts");
   }
 
   function decisionTree() {
     $("#projectText").text("Decision Tree");
+    $("#codeLink").hide();
+    $("#codeShow").text("Code Upon Request");
+    $("#codeDescription").text("Machine Learning implementation of ID3 Algorithm w/ > 95% accuracy on test data");
   }
 
   function tetris() {
     $("#projectText").text("Tetris");
+    $("#codeLink").hide();
+    $("#codeShow").text("Code Upon Request");
+    $("#codeDescription").text("Java application version of classic Tetris w/ acclerated levels and speed");
   }
 
   function finfo() {
     $("#projectText").text("Finfo");
+    $("#codeShow").text("Git Repo: ");
+    $("#codeLink").attr("href", "https://github.com/greysongy/finfo");
+    $("#codeLink").show();
+    $("#codeDescription").text("Site for authenticated users to consolidate financial and travel information");
   }
 
   function pagerank() {
     $("#projectText").text("PageRank");
+    $("#codeLink").hide();
+    $("#codeShow").text("Code Upon Request");
+    $("#codeDescription").text("Implemented directed graph and Google's PageRank algorithm w/ adjacency matrix");
   }
 
   function othello() {
     $("#projectText").text("Othello");
+    $("#codeLink").hide();
+    $("#codeShow").text("Code Upon Request");
+    $("#codeDescription").text("Othello w/ varying levels of AI computer players");
   }
 }
