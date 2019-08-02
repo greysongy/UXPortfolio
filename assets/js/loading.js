0308d5845329afaf23efc14577c4236a4cf1eeb0
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-08-02 10:27:33 -0700

    Removed buggy loading gif

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -15,22 +15,22 @@
 
 // }
 
-function doOnce() {
-    if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-        // setTimeout(showStuff, 1900);
-        document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-        setTimeout(showStuff, 3600);
-    }
-    else {
-        $('#loadContent').css("display", "none");
-        $('#mainContent').show();
-    }
-}
+// function doOnce() {
+//     if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+//         // setTimeout(showStuff, 1900);
+//         document.cookie = "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+//         setTimeout(showStuff, 3600);
+//     }
+//     else {
+//         $('#loadContent').css("display", "none");
+//         $('#mainContent').show();
+//     }
+// }
 
-function showStuff() {
-    $('#loadContent').css("display", "none");
-    $('#mainContent').fadeIn(4000);
+// function showStuff() {
+//     $('#loadContent').css("display", "none");
+//     $('#mainContent').fadeIn(4000);
 
-}
+// }
 
-doOnce();
+// doOnce();
