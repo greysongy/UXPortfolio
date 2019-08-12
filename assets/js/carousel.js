9bed3691eec2a6df142099dc993142ae762ca7ae
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-08-12 11:24:16 -0700

    Added logic to hide/show previous and next buttons for carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,78 +1,78 @@
 var carousel = $(".carousel"),
-    currdeg  = 0;
+  currdeg = 0;
 
 $(".next").on("click", { d: "n" }, rotate);
 $(".prev").on("click", { d: "p" }, rotate);
 
-function rotate(e){
-  if(e.data.d=="n"){
+function rotate(e) {
+  if (e.data.d == "n") {
     currdeg = currdeg - 60;
   }
-  if(e.data.d=="p"){
+  if (e.data.d == "p") {
     currdeg = currdeg + 60;
   }
   carousel.css({
-    "-webkit-transform": "rotateY("+currdeg+"deg)",
-    "-moz-transform": "rotateY("+currdeg+"deg)",
-    "-o-transform": "rotateY("+currdeg+"deg)",
-    "transform": "rotateY("+currdeg+"deg)"
+    "-webkit-transform": "rotateY(" + currdeg + "deg)",
+    "-moz-transform": "rotateY(" + currdeg + "deg)",
+    "-o-transform": "rotateY(" + currdeg + "deg)",
+    "transform": "rotateY(" + currdeg + "deg)"
   });
   console.log("Current Degrees", currdeg);
-  if(currdeg >= 0) {
-    var remainder = currdeg%360;
+  if (currdeg >= 0) {
+    var remainder = currdeg % 360;
     console.log("remainder", remainder);
-    switch(remainder) {
-      case 0: 
+    switch (remainder) {
+      case 0:
         console.log("Best Gig");
         bestGig();
         break;
-      case 60: 
+      case 60:
         console.log("Decision Tree");
         decisionTree();
         break;
-      case 120: 
+      case 120:
         console.log("Fridge");
         fridge();
         break;
-      case 180: 
+      case 180:
         console.log("Finfo");
         finfo();
         break;
-      case 240: 
+      case 240:
         console.log("PageRank");
         pagerank();
         break;
-      case 300: 
+      case 300:
         console.log("Othello");
         othello();
         break;
     }
   }
-  else if(currdeg < 0) {
-    var remainder = Math.abs(currdeg)%360;
-    console.log("neg remainder",remainder);
-    switch(remainder) {
-      case 0: 
+  else if (currdeg < 0) {
+    var remainder = Math.abs(currdeg) % 360;
+    console.log("neg remainder", remainder);
+    switch (remainder) {
+      case 0:
         console.log("BestGig");
         bestGig();
         break;
-      case 60: 
+      case 60:
         console.log("Othello");
         othello();
         break;
-      case 120: 
+      case 120:
         console.log("PageRank");
         pagerank();
         break;
-      case 180: 
+      case 180:
         console.log("Finfo");
         finfo();
         break;
-      case 240: 
+      case 240:
         console.log("Fridge");
         fridge();
         break;
-      case 300: 
+      case 300:
         console.log("Decision Tree");
         decisionTree();
         break;
@@ -123,6 +123,22 @@ function rotate(e){
     $("#projectText").text("Othello");
     $("#codeLink").hide();
     $("#codeShow").text("Code Upon Request");
-    $("#codeDescription").text( "Othello w/ varying levels of AI computer players");
+    $("#codeDescription").text("Othello w/ varying levels of AI computer players");
   }
 }
+
+var dropdown = false
+
+$(".navbar-toggler").on("click", function () {
+  console.log("The small navbar was clicked");
+  if (!dropdown) {
+    $(".next").css("visibility", "hidden");
+    $(".prev").css("visibility", "hidden");
+    dropdown = true;
+  }
+  else {
+    $(".next").css("visibility", "visible");
+    $(".prev").css("visibility", "visible");
+    dropdown = false;
+  }
+})
