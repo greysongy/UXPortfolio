63baa58855e24e8967da1763e989461b857e4a56
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-08-15 18:57:14 -0700

    Restyled layout of connect page

--- assets/js/dissolve.js
+++ assets/js/dissolve.js
@@ -12,8 +12,8 @@ function init() {
     root.renderer.setPixelRatio(window.devicePixelRatio || 1);
     root.camera.position.set(0, 0, 60);
 
-    var width = 100;
-    var height = 60;
+    var width = 75;
+    var height = 45;
 
     var slide = new Slide(width, height, 'out');
     var l1 = new THREE.ImageLoader();
