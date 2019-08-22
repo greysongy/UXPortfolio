17e5b4a194429e3a6511b665ba2e91f7d0c5cee7
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-08-22 13:01:55 -0700

    Addiditional Java projects/conditional rendering for repo links

--- assets/js/technologies.js
+++ assets/js/technologies.js
@@ -12,17 +12,51 @@ transform();
 
 const finfo = {
     name: "Finfo",
-    image: "finfo2.png"
+    image: "finfo2.png",
+    repoLink: "https://github.com/greysongy/finfo",
+    academic: false
 }
 
 const fridge = {
-    name: "Fridge", 
-    image: "RecipePage.PNG"
+    name: "Fridge",
+    image: "RecipePage.PNG",
+    repoLink: "https://github.com/greysongy/virtual-fridge",
+    academic: false
 }
 
 const bestGig = {
-    name: "BestGig", 
-    image: "bgF.png"
+    name: "BestGig",
+    image: "bgF.png",
+    repoLink: "https://github.com/greysongy/BestGig",
+    academic: false
+}
+
+const othello = {
+    name: "Othello",
+    image: "othelloPic.png",
+    repoLink: "",
+    academic: true
+}
+
+const decisionTree = {
+    name: "Decision Tree", 
+    image: "decisionTree.png", 
+    repoLink: "", 
+    academic: true
+}
+
+const pagerank = {
+    name: "PageRank", 
+    image: "pagerank.png", 
+    repoLink: "", 
+    academic: true
+}
+
+const heap = {
+    name: "Heap", 
+    image: "heapPic1.png", 
+    repoLink: "", 
+    academic: true
 }
 
 const tech = [
@@ -31,111 +65,111 @@ const tech = [
         image: "javascript.jpg",
         details: ["Used in tandem with Ajax and Axios for application routing and API integration", "Built distinct front-end UIs with React components and JQuery DOM manipulation", "Coded server-side algorithmic logic for multiple applications"],
         projects: [fridge, bestGig, finfo],
-        descriptions: ["Function component logic, API integration, middleware routing", "Logic to dynamically display/sort results, take in reviews, statically display ML results", "Used in conjunction with JQuery to modify DOM elements/retrieve API calls"]
-    }, 
+        descriptions: ["Function component logic, API integration, middleware routing", "Logic to dynamically display/sort results, take in reviews, statically display ML results", "Dynamic logic to query APIs, conditionally display stored stocks/available flights"]
+    },
     {
         name: "Python",
         image: "python.png",
         details: ["Implemented numerous variations of classic algorithms with strong runtime considerations", "Worked with data structures (examples include stacks, priority queues, dictionaries, BSTs, DAGs, MSTs, etc.)", "Some experience building small-scale applications with Django and Flask"],
         projects: [],
         descriptions: []
-    }, 
+    },
     {
         name: "Java",
         image: "java.jpg",
         details: ["Designed elaborate applications using object-oriented paradigm", "Implemented complex algorithms (machine learning variants, PageRank, etc.) with rigorous unit testing", "Used JavaFX to design GUIs for multiple interactive applications"],
-        projects: [],
-        descriptions: []
-    }, 
+        projects: [othello, decisionTree, pagerank, heap],
+        descriptions: ["Object-Oriented design with over 15 classes to combine game logic and multiple levels of AI opponent", "Recursive logic to mathematically calculate information gain and entropy, before iteratively deciding which attribute to split on", "Implemented directed/undirected graph with adjacency matrix as basis for subsequent Prim-Jarnik and Page Rank algorithms", "Implementation of a priority queue using underlying heap structure with rigorours unit testing"]
+    },
     {
         name: "Node.js",
         image: "nodejs.jpg",
         details: ["Built multiple coupled applications with node facilitating back-end servers", "Used in tandem with multiple npm packages to provide interactive functionality for users", "Unified distinct features of MVC applications under a single programming language paradigm"],
         projects: [fridge, bestGig],
         descriptions: ["Concurrent execution of multiple servers/backend routing", "Provides environment to sync data intake and routing to distinct views"]
-    }, 
+    },
     {
         name: "HTML",
         image: "htmlPic.png",
         details: ["Styled front-end designs with intrinsic HTML elements", "Used in tandem with DOM manipulation libraries like JQuery", "Applied principles to frameworks with similar syntax, like React's JSX"],
-        projects: [bestGig],
-        descriptions: ["Used combination of inherent components and Bootstrap styles to create dynamic interaction"]
-    }, 
+        projects: [bestGig, finfo],
+        descriptions: ["Used combination of inherent components and Bootstrap styles to create dynamic interaction", "Combinations of intrinsic HTML elements and wrapper divs to ensure proper rendering"]
+    },
     {
         name: "React",
         image: "react2.jpg",
         details: ["Built fully React interfaces with complex hierarchies of nested components/state", "Integrated existing React libraries (like Material UI) into full-stack applications", "Working on transitioning web applications to mobile versions with React Native"],
         projects: [fridge],
-        descriptions: ["Integrated with Material UI to dynamically display user ingredients/potential recipes"] 
-    }, 
+        descriptions: ["Integrated with Material UI to dynamically display user ingredients/potential recipes"]
+    },
     {
-        name: "JQuery", 
-        image: "jquery2.png", 
-        details: ["Used to provide DOM manipulation for purely front-end applications", "Combined with back-end server side code to dynamically manipulate client-side views", "Integrated with three dimensional CSS to produce comprehensive aesthetic designs"], 
-        projects: [bestGig], 
-        descriptions: ["Dynamic mixture of posts, puts, and gets to retrieve and append relevant information"]
-    }, 
+        name: "JQuery",
+        image: "jquery2.png",
+        details: ["Used to provide DOM manipulation for purely front-end applications", "Combined with back-end server side code to dynamically manipulate client-side views", "Integrated with three dimensional CSS to produce comprehensive aesthetic designs"],
+        projects: [bestGig, finfo],
+        descriptions: ["Dynamic mixture of posts, puts, and gets to retrieve and append relevant information", "Frontend DOM manipulation/ajax calls to retrieve information relevant to a particular user"]
+    },
     {
-        name: "CSS", 
-        image: "css.png", 
-        details: ["Integrated vast array of specific properties to improve visual appeal of various applications", "Used similar syntax to style React components", "Combined with media queries to ensure mobile responsiveness for multiple web applications"], 
-        projects: [fridge, bestGig], 
-        descriptions: ["Combined inherent React styling with built in Material UI styles", "Flexbox/relative positioning and traditional margin, color, etc. styling"]
-    }, 
+        name: "CSS",
+        image: "css.png",
+        details: ["Integrated vast array of specific properties to improve visual appeal of various applications", "Used similar syntax to style React components", "Combined with media queries to ensure mobile responsiveness for multiple web applications"],
+        projects: [fridge, bestGig, finfo],
+        descriptions: ["Combined inherent React styling with built in Material UI styles", "Flexbox/relative positioning and traditional margin, color, etc. styling", "Combination of flexbox properties, classes, ids, and specific properties to give elements various styles"]
+    },
     {
-        name: "TensorFlow", 
-        image: "tensorflow.png", 
-        details: ["Coded polynomial regression model with multiple layers to estimate Uber fares from ride distance", "Working on building larger layered network to provide more accurate earnings estimates to gig workers", "Taking Deep Learning course that emphasizes building multi-layered neural networks"], 
-        projects: [bestGig], 
+        name: "TensorFlow",
+        image: "tensorflow.png",
+        details: ["Coded polynomial regression model with multiple layers to estimate Uber fares from ride distance", "Working on building larger layered network to provide more accurate earnings estimates to gig workers", "Taking Deep Learning course that emphasizes building multi-layered neural networks"],
+        projects: [bestGig],
         descriptions: ["Built polynomial regression model to estimate an Uber fare from the distance of a trip (modeled after the Tensorflow.js documentation)"]
-    }, 
+    },
     {
-        name: "Bootstrap", 
-        image: "boostrap3.png", 
-        details: ["Combined inherent components with multiple full-stack applications to build smooth UI", "Implemented various versions of inherent grid system to make web applications mobile responsive", "Integrated inherent Bootstrap styling with CSS properties (with mx as an example of the contrasts in syntax)"], 
-        projects: [bestGig], 
-        descriptions: ["Grid system and built in components integrated with overall layout"]
-    }, 
+        name: "Bootstrap",
+        image: "boostrap3.png",
+        details: ["Combined inherent components with multiple full-stack applications to build smooth UI", "Implemented various versions of inherent grid system to make web applications mobile responsive", "Integrated inherent Bootstrap styling with CSS properties (with mx as an example of the contrasts in syntax)"],
+        projects: [bestGig, finfo],
+        descriptions: ["Grid system and built in components integrated with overall layout", "Grid system/intrinsic stling to render elements in appropriate locations"]
+    },
     {
-        name: "Express", 
-        image: "express2.jpg", 
-        details: ["Created back-end servers using express functionality", "Used middleware to facilitate application routing", "Implemented alongside callbacks to ensure proper timing of requests and responses"], 
-        projects: [fridge, bestGig], 
+        name: "Express",
+        image: "express2.jpg",
+        details: ["Created back-end servers using express functionality", "Used middleware to facilitate application routing", "Implemented alongside callbacks to ensure proper timing of requests and responses"],
+        projects: [fridge, bestGig],
         descriptions: ["Constructed middleware for deployed site and production version", "Created a series of different views and routes to provide users with multiple pages of content"]
-    }, 
+    },
     {
-        name: "Firebase", 
-        image: "firebase.jpg", 
-        details: ["Implemented user authentication feature on small scale application", "Stored data associated with users that could be continuously accessed and updated", "Currently learning cloud storage and realtime database features"], 
-        projects: [], 
-        descriptions: []
-    }, 
+        name: "Firebase",
+        image: "firebase.jpg",
+        details: ["Implemented user authentication feature on small scale application", "Stored data associated with users that could be continuously accessed and updated", "Currently learning cloud storage and realtime database features"],
+        projects: [finfo],
+        descriptions: ["User authentication features/storage of their particular saved stocks"]
+    },
     {
-        name: "Mongoose", 
-        image: "mongoose.png", 
-        details: ["Used mongoose ORM to create strict ORM requirements on loose MongoDB data", "Provided relational functionality in a noSQL environment", "Maintained simple Mongo syntax while providing additional framework to structure data"], 
-        projects: [fridge], 
+        name: "Mongoose",
+        image: "mongoose.png",
+        details: ["Used mongoose ORM to create strict ORM requirements on loose MongoDB data", "Provided relational functionality in a noSQL environment", "Maintained simple Mongo syntax while providing additional framework to structure data"],
+        projects: [fridge],
         descriptions: ["Modeled schemas to store user identification/unique identifiers"]
-    }, 
+    },
     {
-        name: "MongoDB", 
-        image: "mongo.png", 
-        details: ["Implemented backend storage features for large-scale applications", "Used loose JSON syntax to store data and associated key-value pairs", 'Combined with ORMs like Mongoose to construct some degree of relational functionality while maintaining the intrinsic benefits of a noSQL database'], 
-        projects: [fridge], 
+        name: "MongoDB",
+        image: "mongo.png",
+        details: ["Implemented backend storage features for large-scale applications", "Used loose JSON syntax to store data and associated key-value pairs", 'Combined with ORMs like Mongoose to construct some degree of relational functionality while maintaining the intrinsic benefits of a noSQL database'],
+        projects: [fridge],
         descriptions: ["Created underlying infrastructure for storing users, fridges, and their ingredients"]
-    }, 
+    },
     {
-        name: "MySQL", 
-        image: "mysql.jpg", 
-        details: ["Used strict relational features to manage backend of complex applications", "Coded queries in both raw SQL and in concert with wrapper ORMs", "Designed networks of relations to ensure scalability and efficiency"], 
-        projects: [bestGig], 
+        name: "MySQL",
+        image: "mysql.jpg",
+        details: ["Used strict relational features to manage backend of complex applications", "Coded queries in both raw SQL and in concert with wrapper ORMs", "Designed networks of relations to ensure scalability and efficiency"],
+        projects: [bestGig],
         descriptions: ["Stored companies, reviews, users and their corresponding values/relationships"]
-    }, 
+    },
     {
-        name: "Sequelize", 
-        image: "sequelize.jpg", 
-        details: ["Simplified lengthy MySQL syntax into smoother format", "Used promise-based flow of control to ensure smooth interactions between frontend and backend of applications", "Maintained benefits of relational database while writing queries in a format more consistent with Javascript"], 
-        projects: [bestGig], 
+        name: "Sequelize",
+        image: "sequelize.jpg",
+        details: ["Simplified lengthy MySQL syntax into smoother format", "Used promise-based flow of control to ensure smooth interactions between frontend and backend of applications", "Maintained benefits of relational database while writing queries in a format more consistent with Javascript"],
+        projects: [bestGig],
         descriptions: ["Leveraged friendlier JSON syntax to create object models with specific definitions"]
     }
 ]
@@ -159,19 +193,32 @@ function displayTech(index) {
     $("#ttitle").text(element.name);
     $("#tlogo").attr('src', "assets/images/" + element.image);
     console.log("projects", element.projects);
-    for(var j = 0; j < element.details.length; j++) {
+    for (var j = 0; j < element.details.length; j++) {
         $("#skillsList").append(`<li>${element.details[j]}</li>`)
     }
     for (var i = 0; i < element.projects.length; i++) {
         console.log("This ran for", i);
-        var newCard = $(`<div class='card'>
+        if (!element.projects[i].academic) {
+            var newCard = $(`<div class='card'>
             <img class= 'card-img-top' src = ${'assets/images/' + element.projects[i].image} alt = 'Card image cap' >
             <div class='card-body'>
                 <h3 class='card-title styledText'>${element.projects[i].name}</h3>
                 <p class='card-text styledText'>${element.descriptions[i]}</p>
-                <a href='#' class='btn btn-primary'>Go somewhere</a>
+                <a href=${element.projects[i].repoLink} target = '_blank' class='btn btn-primary' >Explore the Git Repo</a>
             </div>
     </div > `);
+        }
+        else {
+            var newCard = $(`<div class='card'>
+            <img class= 'card-img-top' src = ${'assets/images/' + element.projects[i].image} alt = 'Card image cap' >
+            <div class='card-body'>
+                <h3 class='card-title styledText'>${element.projects[i].name}</h3>
+                <p class='card-text styledText'>${element.descriptions[i]}</p>
+                <p class='subtext styledText'>Academic Project, Code Upon Request</p>
+            </div>
+    </div > `);
+        }
+
         $("#section" + i).append(newCard);
         // $("section1").append("<p>Test text</p>");
     }
@@ -179,7 +226,7 @@ function displayTech(index) {
 
 function emptyCards() {
     $("#skillsList").empty();
-    for(var i = 0; i < 6; i++) {
+    for (var i = 0; i < 6; i++) {
         $("#section" + i).empty();
     }
 }
